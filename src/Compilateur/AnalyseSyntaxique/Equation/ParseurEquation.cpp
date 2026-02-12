7e1574b8c2390079bc4ccd78aa7235bfde964f49
Zyphorah <zyphorah.zen@gmail.com>
2026-02-11 20:36:18 -0500

    Add token skip logic for instructions to prevent parsing crashes. #8

--- src/Compilateur/AnalyseSyntaxique/Equation/ParseurEquation.cpp
+++ src/Compilateur/AnalyseSyntaxique/Equation/ParseurEquation.cpp
@@ -14,6 +14,11 @@ std::shared_ptr<INoeud> ParseurEquation::parser(std::vector<Token>& tokens, int&
 {
     ConstructeurEquationFlottante constructeurEquation(constructeurArbre);
     auto expression = constructeurEquation.recupererConstructeurArbre()->construire(tokens, index);
-    consommer(tokens, index, TOKEN_POINT_VIRGULE, "Erreur : ';' attendu à la fin de l'instruction");
+
+    if(tokens[index].type != TOKEN_CALL) // skip en cas d'instruction car il contient un parseur qui mange déjà les tokens ce qui provoque un crash
+    {
+        consommer(tokens, index, TOKEN_POINT_VIRGULE, "Erreur : ';' attendu à la fin de l'instruction");
+    }
+
     return expression;
 }
