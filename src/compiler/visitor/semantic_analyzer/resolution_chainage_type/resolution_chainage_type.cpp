ea6eb862dc4b0dcf8ac74b6e4d47302413bc1f4a
Zyphorah <zyphorah.zen@gmail.com>
2026-04-26 10:46:01 -0400

    A minor formality: documentation is usually located in the headers. So, I moved the documentation from the .cpp file to the .h file.

--- src/compiler/visitor/semantic_analyzer/resolution_chainage_type/resolution_chainage_type.cpp
+++ src/compiler/visitor/semantic_analyzer/resolution_chainage_type/resolution_chainage_type.cpp
@@ -11,44 +11,6 @@
 #include <stdexcept>
 #include <string>
 
-/**
- * @brief Example processing: this is a succession of calls on an object like this test.getDepth().getDepthTwo()
- *
- * @details
- * Explanation of type resolution in the call chain:
- * 1. We start by resolving the type of the receiver of the call, that is to say 'test' in the example test.getDepth().getDepthTwo().
- * It is first necessary to understand what receiver means in this context: it is the object on which the method is called, it is the starting point of the call chain, it is the link in the call chain that will allow us to resolve the type of the next call.
- *
- * 2. Then, once we have the type of the receiver, we will query the class registry to find the class corresponding to this type.
- * We use the IType* for a specific reason, that is to say that the IType* is a pointer to a structure that contains information about the type, such as its name and other useful information for type resolution.
- * It can be a complex or simple type, or an array, etc... We need this type to perform the type resolution of the next call in the chain.
- * If I am in the case of getDepthTwo() I need the return type to resolve getDepthTwo() because it is found in a different class from the base test class.
- */
- 
-
- /*                                                                                                                                                                                                                                 
-           ┌─────────────────────────────┌───────────────────────────┐          
-           │NodeCallObject: getDepthTwo()│ TypeResolut (Intermediate)│          
-           └────┬────────▲───────────────└───────────────────────────┘          
-                │        │                                                      
-                │        │                                                      
-                │        │                                                      
-                │        └─────────────────────────┐                            
-            receiver                         provides Type_B                    
-                │                                  │                            
-                │                                  │                            
-                │                                  │                            
-           ┌────▼─────────────────────┌────────────┼──────────────┐             
-           │NodeCallObject: getDepth()│ TypeResolut (Intermediate)│             
-           └────┬────────▲────────────└───────────────────────────┘             
-                │        │                                                      
-            receiver     └────────────────────┐                                 
-                │                      provides Type_A                          
-                │                             │                                 
-           ┌────▼───────────────┌─────────────┼─────────────┐                   
-           │ NodeVariable: test │ TypeResolut (Intermediate)│                   
-           └────────────────────└───────────────────────────┘                                                                                                 
- */
  
 void ResolutionChainageType::visiter(NodeCallObject *nodeCallObject)
 {
