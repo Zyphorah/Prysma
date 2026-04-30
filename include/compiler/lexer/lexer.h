3e43dba03382e54592d5a44d4702292b4b94400c
opcode870 <felno9090@gmail.com>
2026-04-30 14:00:00 -0400

    refactor lexer header to DFA state machine with char class LUT

--- include/compiler/lexer/lexer.h
+++ include/compiler/lexer/lexer.h
@@ -5,20 +5,24 @@
 // SPDX-License-Identifier: GPL-3.0-or-later WITH Prysma-exception-1.0
 //
 //===----------------------------------------------------------------------===//
+//
+// DFA-driven lexer for Prysma source files.
+// Flat transition table indexed by (state, char-class) pairs.
+// Character classes fold the 256-byte alphabet down so the hot
+// data fits in L1. Keywords resolved post-scan via length-bucketed switch.
+//
+//===----------------------------------------------------------------------===//
 
 #ifndef F2D02E97_AF62_409B_84AD_90905E9BE240
 #define F2D02E97_AF62_409B_84AD_90905E9BE240
+
 #include "token_type.h"
-#include <array>
-#include <cstddef>
+#include <cstdint>
 #include <llvm-18/llvm/ADT/StringRef.h>
 #include <string>
-#include <utility>
 #include <vector>
 
 using namespace std;
-// lexer used for tokenizing the prysma source code
-// filters the source code into a list of identified tokens
 
 struct Token {
     TokenType type;
@@ -27,58 +31,72 @@ struct Token {
     int column;
 };
 
+enum DfaState : uint8_t {
+    S_START = 0,
+    S_IDENT,
+    S_INT,
+    S_FLOAT,
+    S_DOT_AFTER_INT,
+    S_STRING,
+    S_STRING_ESC,
+    S_SLASH,
+    S_LINE_CMT,
+    S_BLOCK_CMT,
+    S_BLOCK_STAR,
+    S_EQ,
+    S_BANG,
+    S_LT,
+    S_GT,
+    S_AMP,
+    S_PIPE,
+    S_COLON,
+    S_PLUS,
+    S_MINUS,
+    S_SIGN_INT,
+    S_SIGN_FLOAT,
+    S_SIGN_DOT,
+    NUM_STATES
+};
+
+enum CharClass : uint8_t {
+    CC_OTHER = 0,
+    CC_ALPHA,    // a-z A-Z _
+    CC_DIGIT,    // 0-9
+    CC_DOT,
+    CC_DQUOTE,
+    CC_SLASH,
+    CC_STAR,
+    CC_PLUS,
+    CC_MINUS,
+    CC_PERCENT,
+    CC_EQ,
+    CC_BANG,
+    CC_LT,
+    CC_GT,
+    CC_AMP,
+    CC_PIPE,
+    CC_COLON,
+    CC_LPAREN,
+    CC_RPAREN,
+    CC_LBRACE,
+    CC_RBRACE,
+    CC_LBRACK,
+    CC_RBRACK,
+    CC_SEMI,
+    CC_COMMA,
+    CC_NL,
+    CC_WS,       // space, tab, \r
+    CC_BSLASH,
+    NUM_CLASSES
+};
+
 class Lexer {
-    private:
-
-    // Dictionary for reserved keywords of the prysma programming language
-    // Allows to differentiate identifiers from keywords
-    static constexpr std::array<std::pair<const char*, TokenType>, 31> keywordsArray = {{
-        {"char", TOKEN_TYPE_CHAR},
-        {"arg", TOKEN_ARG},
-        {"fn", TOKEN_FUNCTION},
-        {"if", TOKEN_IF},
-        {"else", TOKEN_ELSE},
-        {"while", TOKEN_WHILE},
-        {"for", TOKEN_FOR},
-        {"return", TOKEN_RETURN},
-        {"true", TOKEN_TRUE},
-        {"false", TOKEN_FALSE},
-        {"scope", TOKEN_SCOPE},
-        {"aff", TOKEN_ASSIGN},
-        {"dec", TOKEN_DECL},
-        {"int64", TOKEN_TYPE_INT64},
-        {"int32", TOKEN_TYPE_INT32},
-        {"float", TOKEN_TYPE_FLOAT},
-        {"string", TOKEN_TYPE_STRING},
-        {"bool", TOKEN_TYPE_BOOL},
-        {"void", TOKEN_TYPE_VOID},
-        {"call", TOKEN_CALL},
-        {"pass", TOKEN_PASS},
-        {"ref", TOKEN_REF},
-        {"unref", TOKEN_UNREF},
-        {"include", TOKEN_INCLUDE},
-        {"new", TOKEN_NEW},
-        {"ptr",TOKEN_TYPE_PTR},
-        {"delete", TOKEN_DELETE},
-        {"class", TOKEN_CLASS},
-        {"public", TOKEN_PUBLIC},
-        {"private", TOKEN_PRIVATE},
-        {"protected",TOKEN_PROTECTED},
-    }};
-
-    // Private functions for tokenization
-    static void addCurrentWord(llvm::StringRef currentWord, vector<Token>& tokens, int line, int column);
-    static void handleOperatorsAndDelimiters(char current, const string& sourceCode, size_t& pos, vector<Token>& tokens, int line, int& column);
-    static void handleMathOperators(char current, vector<Token>& tokens, int line, int column);
-    static void handleDelimiters(char current, vector<Token>& tokens, int line, int column);
-    static void handleComplexOperators(char current, const string& sourceCode, size_t& pos, vector<Token>& tokens, int line, int& column);
-    static void handleLiterals(char current, const string& sourceCode, size_t& pos, vector<Token>& tokens, int line, int& column);
-    static void handleComments(const string& sourceCode, size_t& pos);
-    static void handleNumber(const string& sourceCode, size_t& pos, vector<Token>& tokens, int line, int& column);
-    static auto isNegativeNumberContext(const vector<Token>& tokens) -> bool;
-
-    public:
+public:
     static auto tokenize(const string& sourceCode) -> vector<Token>;
+
+private:
+    static TokenType resolveKeyword(llvm::StringRef word);
+    static bool isUnaryContext(const vector<Token>& tokens);
 };
 
 #endif /* F2D02E97_AF62_409B_84AD_90905E9BE240 */
