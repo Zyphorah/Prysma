ec1d6db7aa9fecce540ebee76917e0f26dad390d
opcode870 <felno9090@gmail.com>
2026-04-30 14:20:00 -0400

    optimize lexer performance with pointer arithmetic, tight loops, and word-sized keyword loads

--- include/compiler/lexer/lexer.h
+++ include/compiler/lexer/lexer.h
@@ -31,33 +31,6 @@ struct Token {
     int column;
 };
 
-enum DfaState : uint8_t {
-    S_START = 0,
-    S_IDENT,
-    S_INT,
-    S_FLOAT,
-    S_DOT_AFTER_INT,
-    S_STRING,
-    S_STRING_ESC,
-    S_SLASH,
-    S_LINE_CMT,
-    S_BLOCK_CMT,
-    S_BLOCK_STAR,
-    S_EQ,
-    S_BANG,
-    S_LT,
-    S_GT,
-    S_AMP,
-    S_PIPE,
-    S_COLON,
-    S_PLUS,
-    S_MINUS,
-    S_SIGN_INT,
-    S_SIGN_FLOAT,
-    S_SIGN_DOT,
-    NUM_STATES
-};
-
 enum CharClass : uint8_t {
     CC_OTHER = 0,
     CC_ALPHA,    // a-z A-Z _
@@ -85,8 +58,7 @@ enum CharClass : uint8_t {
     CC_SEMI,
     CC_COMMA,
     CC_NL,
-    CC_WS,       // space, tab, \r
-    CC_BSLASH,
+    CC_WS,
     NUM_CLASSES
 };
 
@@ -96,7 +68,6 @@ public:
 
 private:
     static TokenType resolveKeyword(llvm::StringRef word);
-    static bool isUnaryContext(const vector<Token>& tokens);
 };
 
 #endif /* F2D02E97_AF62_409B_84AD_90905E9BE240 */
